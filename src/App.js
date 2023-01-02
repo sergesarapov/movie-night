0e156b79cee53542dadaec02d3b77c2f282a5023
Sergei Sarapov <sarapov.ss@gmail.com>
2023-01-02 16:06:50 +0200

    Formatted files with 4 spaces

--- src/App.js
+++ src/App.js
@@ -11,42 +11,42 @@ const socket = socketIOClient(ENDPOINT);
 const roomId = uuid();
 
 export const App = () => {
-  const [loading, setIsLoading] = useState(true);
-  const [mainLoading, setIsMainLoading] = useState(true);
+    const [loading, setIsLoading] = useState(true);
+    const [mainLoading, setIsMainLoading] = useState(true);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const resp = await fetch('https://movie-night-server.onrender.com');
-      if (resp.status === 200) {
-        setIsMainLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
+    useEffect(() => {
+        const fetchData = async () => {
+            const resp = await fetch('https://movie-night-server.onrender.com');
+            if (resp.status === 200) {
+                setIsMainLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
 
-  const onCreateNewRoom = async () => {
-    const resp = await fetch('https://raw.githubusercontent.com/sergesarapov/movie-night-api-mock/main/250.json');
-    const response = await resp.json();
-    const movies = response.items;
-    movies.splice(100, Infinity);
-    socket.emit('created room', { roomId, movies: movies });
-    console.log(`Room ${roomId} created. Requested data.`);
-    setIsLoading(false);
-  }
+    const onCreateNewRoom = async () => {
+        const resp = await fetch('https://raw.githubusercontent.com/sergesarapov/movie-night-api-mock/main/250.json');
+        const response = await resp.json();
+        const movies = response.items;
+        movies.splice(100, Infinity);
+        socket.emit('created room', { roomId, movies: movies });
+        console.log(`Room ${roomId} created. Requested data.`);
+        setIsLoading(false);
+    }
 
-  return (
-    <>
-      <Typography variant="h2" sx={{
-        marginLeft: '16px'
-      }}>Movie Night</Typography>
-      {mainLoading ? (
-        <Typography sx={{ paddingLeft: '16px' }} variant='h6'>Starting the app...</Typography>
-      ) : (<BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home roomId={roomId} newRoomHandler={onCreateNewRoom} />} />
-          <Route path={`/party/:roomId`} element={<Room loading={loading} onLoading={setIsLoading} socket={socket} />} />
-        </Routes>
-      </BrowserRouter>)}
-    </>
-  );
+    return (
+        <>
+            <Typography variant="h2" sx={{
+                marginLeft: '16px'
+            }}>Movie Night</Typography>
+            {mainLoading ? (
+                <Typography sx={{ paddingLeft: '16px' }} variant='h6'>Starting the app...</Typography>
+            ) : (<BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home roomId={roomId} newRoomHandler={onCreateNewRoom} />} />
+                    <Route path={`/party/:roomId`} element={<Room loading={loading} onLoading={setIsLoading} socket={socket} />} />
+                </Routes>
+            </BrowserRouter>)}
+        </>
+    );
 }
--- src/Room.js
+++ src/Room.js
@@ -13,165 +13,165 @@ import Link from '@mui/material/Link';
 import { Link as RouterLink } from 'react-router-dom';
 
 const Alert = forwardRef(function Alert(props, ref) {
-  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
 export const Room = ({ loading = true, onLoading, socket }) => {
-  const { roomId } = useParams();
-  const [movies, updateMovies] = useState([]);
-  const [open, setOpen] = useState(false);
-  const [kickedMovie, updateKickedMovie] = useState();
-  const [isConnected, setIsConnected] = useState(socket.connected);
+    const { roomId } = useParams();
+    const [movies, updateMovies] = useState([]);
+    const [open, setOpen] = useState(false);
+    const [kickedMovie, updateKickedMovie] = useState();
+    const [isConnected, setIsConnected] = useState(socket.connected);
 
-  const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-    setOpen(false);
-  };
-
-  useEffect(() => {
-    socket.on('connect', () => {
-      console.log('Connected');
-      setIsConnected(true);
-    });
-
-    socket.on('disconnect', (data) => {
-      console.log('Disconnected', data);
-      setIsConnected(false);
-    });
-
-    socket.on('send content', (data) => {
-      updateMovies(data);
-      console.log('Recieved the latest data', data.length);
-      if (loading) {
-        onLoading(false);
-      }
-    });
-
-    socket.on('list updated', (title) => {
-      updateKickedMovie(title);
-      setOpen(true);
-      console.log('List updated, movie kicked:', title);
-    });
-
-    socket.emit('connected', roomId);
-    console.log('Emitted joining the room', roomId);
-
-    socket.emit('requested content', roomId);
-    console.log('Emitted request for content');
-
-    return () => {
-      socket.off('connect');
-      socket.off('disconnect');
-      socket.off('send content');
-      socket.off('list updated');
+    const handleClose = (event, reason) => {
+        if (reason === 'clickaway') {
+            return;
+        }
+        setOpen(false);
     };
-  }, [loading, socket, isConnected]);
 
-  const handleDelete = (e) => {
-    socket.emit('delete item', { roomId, movieId: e.target.id });
-  }
+    useEffect(() => {
+        socket.on('connect', () => {
+            console.log('Connected');
+            setIsConnected(true);
+        });
 
-  const url = window.location.href;
+        socket.on('disconnect', (data) => {
+            console.log('Disconnected', data);
+            setIsConnected(false);
+        });
 
-  return (
-    <>
-      <Box sx={{
-        marginLeft: '16px',
-        marginBottom: '16px',
-      }}>
-        <Box sx={{
-          marginBottom: '16px'
-        }}>
-          <RouterLink to="/">Back to main page</RouterLink>
-        </Box>
-        <Typography variant="h6">Invite your friends using this link:</Typography>
-        <TextField
-          variant="outlined"
-          readOnly
-          sx={{
-            width: '200px',
-            height: '36.5px',
-            marginRight: '8px',
-            '& .MuiInputBase-input': {
-              paddingTop: 0,
-              paddingBottom: 0,
-              height: '36px'
+        socket.on('send content', (data) => {
+            updateMovies(data);
+            console.log('Received the latest data', data.length);
+            if (loading) {
+                onLoading(false);
             }
-          }}
-          value={url}
-          id="myInput"
-        />
-        <Button variant="outlined" onClick={() => navigator.clipboard.writeText(url)}>Copy link</Button>
-      </Box>
-      {!loading && <Box sx={{ display: 'flex', marginLeft: '16px' }}>
-        <Typography sx={{
-          paddingRight: '8px'
-        }}>Movies left:</Typography>
-        <Box sx={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          width: '25px',
-          height: '25px',
-          borderRadius: '50%',
-          backgroundColor: '#1976d2',
-          color: 'white'
-        }}>
-          <Typography>{movies.length}</Typography>
-        </Box>
-      </Box>}
-      {loading && <Typography sx={{ padding: '16px' }} variant="h5">Loading movies...</Typography>}
-      <Snackbar open={open} autoHideDuration={2000} onClose={handleClose}>
-        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
-          {`Someone kicked out`} <b>{kickedMovie}</b>
-        </Alert>
-      </Snackbar>
-      {movies.length === 1 && <h3>Your movie for today:</h3>}
-      <Box sx={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        margin: '16px'
-      }}>
-        {movies.map((movie, i) => (
-          <MovieItem key={i} title={movie.title} id={movie.id} image={movie.image} rating={movie.imDbRating} onDelete={handleDelete} isLast={movies.length === 1} />
-        ))}
-      </Box>
-    </>)
+        });
+
+        socket.on('list updated', (title) => {
+            updateKickedMovie(title);
+            setOpen(true);
+            console.log('List updated, movie kicked:', title);
+        });
+
+        socket.emit('connected', roomId);
+        console.log('Emitted joining the room', roomId);
+
+        socket.emit('requested content', roomId);
+        console.log('Emitted request for content');
+
+        return () => {
+            socket.off('connect');
+            socket.off('disconnect');
+            socket.off('send content');
+            socket.off('list updated');
+        };
+    }, [loading, socket, isConnected]);
+
+    const handleDelete = (e) => {
+        socket.emit('delete item', { roomId, movieId: e.target.id });
+    }
+
+    const url = window.location.href;
+
+    return (
+        <>
+            <Box sx={{
+                marginLeft: '16px',
+                marginBottom: '16px',
+            }}>
+                <Box sx={{
+                    marginBottom: '16px'
+                }}>
+                    <RouterLink to="/">Back to main page</RouterLink>
+                </Box>
+                <Typography variant="h6">Invite your friends using this link:</Typography>
+                <TextField
+                    variant="outlined"
+                    readOnly
+                    sx={{
+                        width: '200px',
+                        height: '36.5px',
+                        marginRight: '8px',
+                        '& .MuiInputBase-input': {
+                            paddingTop: 0,
+                            paddingBottom: 0,
+                            height: '36px'
+                        }
+                    }}
+                    value={url}
+                    id="myInput"
+                />
+                <Button variant="outlined" onClick={() => navigator.clipboard.writeText(url)}>Copy link</Button>
+            </Box>
+            {!loading && <Box sx={{ display: 'flex', marginLeft: '16px' }}>
+                <Typography sx={{
+                    paddingRight: '8px'
+                }}>Movies left:</Typography>
+                <Box sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    width: '25px',
+                    height: '25px',
+                    borderRadius: '50%',
+                    backgroundColor: '#1976d2',
+                    color: 'white'
+                }}>
+                    <Typography>{movies.length}</Typography>
+                </Box>
+            </Box>}
+            {loading && <Typography sx={{ padding: '16px' }} variant="h5">Loading movies...</Typography>}
+            <Snackbar open={open} autoHideDuration={2000} onClose={handleClose}>
+                <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+                    {`Someone kicked out`} <b>{kickedMovie}</b>
+                </Alert>
+            </Snackbar>
+            {movies.length === 1 && <h3>Your movie for today:</h3>}
+            <Box sx={{
+                display: 'flex',
+                flexWrap: 'wrap',
+                margin: '16px'
+            }}>
+                {movies.map((movie, i) => (
+                    <MovieItem key={i} title={movie.title} id={movie.id} image={movie.image} rating={movie.imDbRating} onDelete={handleDelete} isLast={movies.length === 1} />
+                ))}
+            </Box>
+        </>)
 }
 
 const MovieItem = ({ title, rating, image, id, onDelete, isLast }) => {
-  return (
-    <Card sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'space-between',
-      width: 155,
-      height: 300,
-      margin: '0 16px 16px 0'
-    }}>
-      <Box sx={{
-        width: '100%',
-        overflow: 'hidden'
-      }}>
-        <img width="100%" src={image} alt="poster" />
-      </Box>
-      <CardContent sx={{
-        padding: '8px'
-      }}>
-        <Link target="_blank" href={`https://www.imdb.com/title/${id}`}>
-          <Typography variant="body2" component="div">
-            {title}
-          </Typography>
-        </Link>
-        <Typography variant="body2" color="text.secondary">
-          {rating}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        {!isLast && <Button variant="contained" id={id} onClick={onDelete} size="large">KICK OUT</Button>}
-      </CardActions>
-    </Card >
-  )
+    return (
+        <Card sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            width: 155,
+            height: 300,
+            margin: '0 16px 16px 0'
+        }}>
+            <Box sx={{
+                width: '100%',
+                overflow: 'hidden'
+            }}>
+                <img width="100%" src={image} alt="poster" />
+            </Box>
+            <CardContent sx={{
+                padding: '8px'
+            }}>
+                <Link target="_blank" href={`https://www.imdb.com/title/${id}`}>
+                    <Typography variant="body2" component="div">
+                        {title}
+                    </Typography>
+                </Link>
+                <Typography variant="body2" color="text.secondary">
+                    {rating}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                {!isLast && <Button variant="contained" id={id} onClick={onDelete} size="large">KICK OUT</Button>}
+            </CardActions>
+        </Card >
+    )
 }
